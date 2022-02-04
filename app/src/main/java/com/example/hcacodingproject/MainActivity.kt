8cda44b215b4d3f5efe81bdc21f4fce7c3ffcddd
blackprogrammer234 <boundstaylor@gmail.com>
2022-02-03 21:59:49 -0500

    Added some layout and updated the gradle to show a sample view of a stack question

--- app/src/main/java/com/example/hcacodingproject/MainActivity.kt
+++ app/src/main/java/com/example/hcacodingproject/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.hcacodingproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.hcacodingproject.databinding.ActivityMainBinding
+//This import is needed to extend the other layout
+import kotlinx.android.synthetic.main.activity_main.*
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        stack_questions_list.layoutManager = LinearLayoutManager(this)
+        stack_questions_list.setHasFixedSize(true)
+
+        val binding: ViewDataBinding? = DataBindingUtil.setContentView(
+            this, R.layout.layout_stack_list_item)
+
+
     }
 }
